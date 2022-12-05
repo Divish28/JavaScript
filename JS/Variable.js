847a6fa21d2cc4c2f72ad8985e7a2caf04c4bfd6
Divish28 <divish.karthik@aspiresys.com>
2022-12-05 17:18:47 +0530

    value_changed

--- JS/Variable.js
+++ JS/Variable.js
@@ -10,3 +10,15 @@ typeof myName;
 console.log(typeof(myName))*/
 let dog = { name : 'Spot', breed : 'Dalmatian'};
 console.log(dog.name)
+
+//using var is a global variable
+//var keyword can be decleared again to change the value of the variable
+var FirstName="Divish"
+var FirstName="Kumar"
+console.log(FirstName)
+
+//const cannot be changed
+const LastName="Kd"
+LastName="KC"
+const LastName="KC"
+console.log(LastName)
