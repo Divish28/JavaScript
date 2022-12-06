bd0cfb15018ff55014d5554f0ff5e1d30370b1dd
Divish28 <divish.karthik@aspiresys.com>
2022-12-06 09:57:38 +0530

    updated

--- JS/Variable.js
+++ JS/Variable.js
@@ -1,14 +1,10 @@
-/*Name="Divish";
-agr=22;
-function display(){
-    console.log(Name)
-}
-display();
-var Name
-let myName=3>7;
-typeof myName;
-console.log(typeof(myName))*/
-let dog = { name : 'Spot', breed : 'Dalmatian'};
+//We can change the value of the variable using let keyword.
+//we cannot redeclare the same variable. 
+let myName="divish"
+myName="Divi"
+console.log(myName)
+
+let dog = { name : 'Spot', breed : 'Dalmatian'}
 console.log(dog.name)
 
 //using var is a global variable
@@ -17,8 +13,13 @@ var FirstName="Divish"
 var FirstName="Kumar"
 console.log(FirstName)
 
-//const cannot be changed
-const LastName="Kd"
-LastName="KC"
-const LastName="KC"
+//const keyword
+const LastName="KD"
 console.log(LastName)
+
+//const variable cannot be changed or redecleared.
+/*const LastName="Kd"
+LastName="KC"
+console.log(LastName)
+const LastName="KC"
+console.log(LastName)*/
