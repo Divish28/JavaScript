418e98f3bfa70b17cf7097164ca7dc8737f636cd
Divish28 <divish.karthik@aspiresys.com>
2022-12-12 11:24:44 +0530

    update

--- JS/objects/objectDeclare.js
+++ JS/objects/objectDeclare.js
@@ -1,9 +1,11 @@
 //Declearing an object
+const person={};
+console.log(person);
+
+//Declearing an object with properties(key:'pair')
 let firstObject={id:'2',role:'trainee'}
 console.log("This is object decleration "+ firstObject.id +" "+ firstObject.role)
 
 //storing multiple values in same key in object
 var object={firstName:['Divish','vijay'] , age:[22,23]};
-console.log(object.age[1]);
-
-//
+console.log(object.firstName[1],object.age[1]);
